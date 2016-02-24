e598033780f55f08930beefcede4cb778e73feeb
Shruti Ganapathy Subramanian <shruti@Shrutis-MacBook-Pro.local>
2016-02-24 15:59:18 +0530

    Image download corrected

--- ZalandoCrawler.py
+++ ZalandoCrawler.py
@@ -20,26 +20,27 @@ class Zalando:
         self.set_directory()
         self.BASE_URL = "https://www.zalando.co.uk"
         self.initial_page = ['https://www.zalando.co.uk/womens-clothing','https://www.zalando.co.uk/mens-clothing']
-        self.parameters = ['women','men']
+        self.parameters = ['Women','Men']
         self.subcategories = [dict() for x in range(2)]
-        authenticate("192.168.1.254:7474","neo4j","priyesh")
+        authenticate("localhost:7474","neo4j","awdr.;/")
         self.zalando_graph = Graph()
-        self.zalando_graph.schema.create_uniqueness_constraint('Zalando', 'article_number')
+        try:
+            self.zalando_graph.schema.create_uniqueness_constraint('Zalando', 'article_number')
+        except Exception as e:
+            self.logger.log("uniqueness constraint already set\n")
         self.count = 0
         self.xml_root = Element('Properties')
         self.properties = open("clothing_properties.xml","w")
-"""
+
         self.logger.write("Zalando Object: " + str(self)+"\n")
 
         for i in range(len(self.initial_page)):
-            self.c = 1
             page_html = requests.get(self.initial_page[i]).content
             page_soup = BeautifulSoup(page_html,"lxml")
             self.get_clothing_attributes(page_soup,i)
             self.build_zalando_database(page_soup,self.parameters[i])
             self.set_attributes(i)
-        self.properties.write(tostring(self.xml_root)) """
-
+        self.properties.write(tostring(self.xml_root))
 
     def set_directory(self):
         print "Setting up directory"
@@ -57,34 +58,32 @@ class Zalando:
         #create xml files for attributes
 
     def set_attributes(self,i):
-        count = 5
         for key in self.subcategories[i]:
-            if count > 0 :
-                product_type = self.subcategories[i][key].split("*")[0]
-                next_page =   self.BASE_URL + self.subcategories[i][key].split("*")[1]
-                count = count - 1
-                while next_page is not None :
-                    try:
-                        attribute_soup = BeautifulSoup(requests.get(next_page).content,"lxml")
-                    except Exception as e:
-                        attribute_soup = BeautifulSoup(requests.get(next_page).content,"lxml")
-                    catalog_articles_list = attribute_soup.find("ul","catalogArticlesList")
-                    for li in catalog_articles_list.findAll("li"):
-                        if str(type(li))=="<class 'bs4.element.Tag'>":
-                            product_id = li['id']
-                            try:
-                                result = self.zalando_graph.cypher.execute("Match (n:Women) where n.article_number = '%s' set n.%s = '%s' " % (product_id,product_type,key))
-                                print "result %s %s %s" %(str(result),product_id,key)
-                            except Exception as e:
-                                print e
-                    if attribute_soup.find("a","catalogPagination_button catalogPagination_button-next") is not None:
-                        next_page = self.BASE_URL + attribute_soup.find("a","catalogPagination_button catalogPagination_button-next")['href']
-                    else:
-                        next_page = None
+            print self.subcategories[i][key].split("*")[1]
+            product_type = self.subcategories[i][key].split("*")[0]
+            next_page =   self.BASE_URL + self.subcategories[i][key].split("*")[1]
+            while next_page is not None :
+                try:
+                    attribute_soup = BeautifulSoup(requests.get(next_page).content,"lxml")
+                except Exception as e:
+                    attribute_soup = BeautifulSoup(requests.get(next_page).content,"lxml")
+                catalog_articles_list = attribute_soup.find("ul","catalogArticlesList")
+                for li in catalog_articles_list.findAll("li"):
+                    if str(type(li))=="<class 'bs4.element.Tag'>":
+                        product_id = li['id']
+                        try:
+                            result = self.zalando_graph.cypher.execute("Match (n:Women) where n.article_number = '%s' set n.%s = '%s' " % (product_id,product_type,key))
+                        except Exception as e:
+                            print e
+                if attribute_soup.find("a","catalogPagination_button catalogPagination_button-next") is not None:
+                    next_page = self.BASE_URL + attribute_soup.find("a","catalogPagination_button catalogPagination_button-next")['href']
+                else:
+                    next_page = None
 
 
 
     def get_product_details(self,product_url,gender):
+
         print self.count
         self.count = self.count + 1
         try:
@@ -101,7 +100,6 @@ class Zalando:
     	product_info["article_number"] = article_span.string
     	product_info["brand"] = brand.string
         product_info["gender"] = gender
-        product_info["attributes"] = ""
 
     	ul = article_span.parent.parent
     	self.logger.write("%s being explored" %(article_span.string)+"\n")
@@ -111,21 +109,33 @@ class Zalando:
     			if(len(li.string.split(":")) > 1 ):
     				product_info[li['class'][0]] = li.string.split(":")[1].encode("ascii","ignore")
 
-    	n = Node(gender,"Zalando")
+    	n = Node(gender,"Zalando","Product")
     	for key in product_info:
     		n.properties[key] = product_info[key]
-    	self.zalando_graph.create(n)
 
-    """
-        Create a directory for each file being created
-        while (image is not None):
-    		url = image['src']
-    		fileName = url.split('/')[-1]
-    		url = url.replace("detail","large")
-    		r = requests.get(url)
-    		n.properties[fileName] = r.content
-    		image = image.nextSibling.nextSibling
-    """
+        while (product_image is not None):
+            url = product_image['src']
+            file_name = url.split('/')[-1]
+            folder_name = file_name.split('@')[0]
+            if os.path.exists(folder_name):
+                break
+            os.makedirs(folder_name)
+            os.chdir(folder_name)
+            url = url.replace("detail","large")
+            try:
+                r = requests.get(url)
+            except Exception as e:
+                r = requests.get(url)
+            open(file_name,"w").write(r.content)
+            os.chdir("..")
+            n["file_system_url"] = "Zalando/"+folder_name
+            n["image_url"] = url
+            product_image = product_image.nextSibling.nextSibling
+        try:
+            self.zalando_graph.create(n)
+        except Exception as e:
+            self.logger.write("Node %s already exists" %(article_span.string))
+
 
     def get_clothing_attributes(self,page_soup,i):
         gender_dictionary = dict()
@@ -170,17 +180,14 @@ class Zalando:
 
 
     def build_zalando_database(self,page_soup,gender):
-
         next_page = self.BASE_URL + page_soup.find("a","catalogPagination_button catalogPagination_button-next")['href']
-
         catalog_articles_list = page_soup.find("ul","catalogArticlesList")
         for li in catalog_articles_list.findAll("li"):
-    		a = li.find("a","catalogArticlesList_productBox")
-    		self.logger.write(self.BASE_URL+a["href"]+" being explored \n")
-    		self.get_product_details(self.BASE_URL+a["href"],gender)
-        self.c = self.c - 1
-        while(self.c == 1 ):
-            self.build_zalando_database(BeautifulSoup(requests.get(next_page).content,"lxml"),gender)
+            a = li.find("a","catalogArticlesList_productBox")
+            self.logger.write(self.BASE_URL+a["href"]+" being explored \n")
+            self.get_product_details(self.BASE_URL+a["href"],gender)
+
+        self.build_zalando_database(BeautifulSoup(requests.get(next_page).content,"lxml"),gender)
 
 print "Creating Zalando Object"
 Zalando()
